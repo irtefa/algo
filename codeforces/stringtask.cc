8e76d1e7f94388fab92cea3a4d6eb9c12c0a9f36
Mohd Irtefa <irtefa1@illinois.edu>
2013-07-26 17:57:44 -0700

    wrote a lot better implementation of stringtask

--- codeforces/stringtask.cc
+++ codeforces/stringtask.cc
@@ -1,21 +1,13 @@
-#include <stdio.h>
 #include <iostream>
-#include <string>
-#include <ctype.h>
 
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-
-    for(int i=0; i < s.size(); i++) {
-        if(s[i] == 'A' || s[i] == 'a' || s[i] == 'e' || s[i] == 'E' || s[i] == 'I' || s[i] == 'i' || s[i] == 'o' || s[i] == 'O' || s[i] == 'U' || s[i] == 'u' || s[i] == 'Y' || s[i] == 'y');
-        else {
-            char c = s[i];
-            putchar ('.');
-            putchar (tolower(c));
+    char c;
+    while (cin >> c) {
+        if(!strchr("AEIOUY", toupper(c))) {
+          cout << '.' << (char) tolower(c);
         }
-    }
+   }
     return 0;
 }
